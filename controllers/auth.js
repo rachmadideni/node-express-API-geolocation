41e3a85c44686008e7acbf2bb2f341b06031244a
Denny Rachmadi <denirachmadi82@gmail.com>
2019-07-10 19:46:10 +0800

    fixed error message login jika rest api tidak jalan

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,34 +15,36 @@ exports.signin = (req,res) => {
 
 	user.findOne({ where: { usernm: username, passwd: password } }).then(results => {
 	    
-	    if(!results){
+	    console.log(results);
+	    if(!!results){
 	    	response.error(results,res)
+	    }else{	    	
+			    try{		    
+				    const { iduser,usernm,user_category } = results.dataValues;
+				    
+				    // create token
+				    const token = jwt.sign({
+							iduser:iduser,
+							username:usernm,
+							user_category:user_category
+						},process.env.token_secret,{
+							expiresIn:"1200000" // 20 menit
+						});
+
+						const data = {
+							iduser,
+							username:usernm,
+							user_category,
+							token
+						}
+						
+						response.ok(data,res)
+
+			    }catch(err){	    	
+						response.error('error creating user token',res);	    	
+			    }
 	    }
 
-	    try{		    
-		    const { iduser,usernm,user_category } = results.dataValues;
-		    
-		    // create token
-		    const token = jwt.sign({
-					iduser:iduser,
-					username:usernm,
-					user_category:user_category
-				},process.env.token_secret,{
-					expiresIn:"1200000" // 20 menit
-				});
-
-				const data = {
-					iduser,
-					username:usernm,
-					user_category,
-					token
-				}
-				
-				response.ok(data,res)
-
-	    }catch(err){	    	
-				response.error('error creating user token',res);	    	
-	    }
 
 	})
 
